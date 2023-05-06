046b1496b8b8e5abbb99ecae57e72fc4444953b9
JG1ANDONLY <72232232+JG1ANDONLY@users.noreply.github.com>
2023-05-06 19:22:21 -0400

    Updated storm_map.

--- R/storm_map.R
+++ R/storm_map.R
@@ -18,11 +18,16 @@ storm_map <- function(storm.ids) {
   track <- hurdat[hurdat$id %in% storm.ids, ]
   map_states <- map_data("state")
 
-  p <- ggplot(track, aes(x = numeric.longitude, y = numeric.latitude, group = id, color = max.wind)) + geom_path(size = 1) + scale_color_gradient(low = "blue", high = "red") + theme_minimal() + theme(panel.grid = element_blank())
-  p <- p + coord_fixed(1.3)
-  p <- p + geom_polygon(data = map_states, aes(x = long, y = lat, group = group), fill = NA, color = "gray50")
-  p <- p + labs(title = "Storm Tracks")
-  p <- p + labs(x = "Longitude", y = "Latitude")
-
+  p <- ggplot(track, aes(x = numeric.longitude, y = numeric.latitude, group = id,
+                         color = max.wind)) +
+    geom_path(size = 1) +
+    scale_color_gradient(low = "blue", high = "red") +
+    theme_minimal() +
+    theme(panel.grid = element_blank()) +
+    coord_fixed(1.3) +
+    geom_polygon(data = map_states, aes(x = long, y = lat, group = group),
+                 fill = NA, color = "gray50") +
+    labs(title = paste("Storm", storm.ids, "Track")) +
+    labs(x = "Longitude", y = "Latitude")
   return(p)
 }
